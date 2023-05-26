9fb7c9e2b80e692895c9e1556fc734e8b8595f72
Isaac Neufing <120573812+Tqpdt67@users.noreply.github.com>
2023-05-26 08:56:32 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -21,11 +21,3 @@ def view_quotes(quotes):
     for quote in quotes:
         print_quote(quote)
 
-def display_quotes(quotes, count):
-    if count >= len(quotes):
-        print("All Quotes:")
-        view_quotes(quotes)
-    else:
-        print(f"First {count} Quotes:")
-        for i in range(count):
-            print_quote(quotes[i])
